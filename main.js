2dc0ce95ca0e03846c68972b9eb117d00db1882b
Ian Snider <irsnider13@gmail.com>
2025-12-11 10:23:10 -0500

    Fixed name and date text

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const button = document.getElementById("share");
 
 button.addEventListener("click", function(){
-    document.getElementById("shareLinks").style.display = "block";
-    document.getElementById("name").style.display = "none";
-    document.getElementById("shareLinks").style.backgroundColor = "hsl(217, 19%, 35%)";
-    document.getElementById("shareLinks").style.color = "white";
+    if(document.getElementById("name").style.display !== "none"){
+        document.getElementById("shareLinks").style.display = "block";
+        document.getElementById("name").style.display = "none";
+        document.getElementById("shareLinks").style.backgroundColor = "hsl(217, 19%, 35%)";
+        document.getElementById("shareLinks").style.color = "white";
+    }else{
+        document.getElementById("shareLinks").style.display = "none";
+        document.getElementById("name").style.display = "flex";
+    }
+    
 });

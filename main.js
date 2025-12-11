275b86b676487321e281d5584f61d44f47ca2587
Ian Snider <irsnider13@gmail.com>
2025-12-11 14:30:13 -0500

    Added button conditional and styling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("share");
 
 button.addEventListener("click", function(){
-    if(document.getElementById("name").style.display !== "none"){
+    if(document.getElementById("name").style.display !== "none" && screen.width <= 850){
         document.getElementById("shareLinks").style.display = "block";
         document.getElementById("name").style.display = "none";
         document.getElementById("shareLinks").style.backgroundColor = "hsl(217, 19%, 35%)";
